a50247221ee1c9f7c11fe7757c7801a0a2febc86
2008Nithin <66758150+2008Nithin@users.noreply.github.com>
2021-03-30 19:57:59 -0400

    Started Javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+let apple = document.getElementById('apple-website');
+let meet = document.getElementById('google-meet')
+let three = document.getElementById('3d-animations');
+
+let iframes = ['C:\\Users\\kotap\\Desktop\\Codin\\Google Meets Styles\\index.html']
+
+
+
+let addIframe = (theelement) => {
+    
+}
+
+apple.addEventListener('click', addIframe, false)
+
+
+
